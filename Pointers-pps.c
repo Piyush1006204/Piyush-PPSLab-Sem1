8ee0a3df659af5ab65fda656be63ed72b2604488
Piyush1006204 <piyushjoshi2150@gmail.com>
2024-12-01 06:22:10 +0530

    Added types of pointer syntax

--- Pointers-pps.c
+++ Pointers-pps.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
     int a=10;
     int *ptr;
     ptr=&a;
-    printf("Value of a :%d",*ptr);
+    printf("Address of a :%x\n",a);
+    printf("Address of a :%x\n",&a);
+    printf("Address of a :%x\n",ptr);
+    printf("Value of a :%d\n",a);
+    printf("Value of a :%d\n",*ptr);
+    a=20;
+    printf("Value of a :%d\n",a);
+    *ptr=4927;
+    printf("Value of a :%d\n",*ptr);
+    printf("Value of a :%d\n",*(&a));
 }
