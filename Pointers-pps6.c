ef32e530c94a1c66edf2b3f8ea89c42d40d5e63b
Piyush1006204 <piyushjoshi2150@gmail.com>
2024-12-02 05:34:12 +0530

    Pointers

---
+++ Pointers-pps6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b;
+    int *ptra,*ptrb;
+    printf("Enter two values :");
+    scanf("%d",a,b);
+    ptra=&a;
+    ptrb=&b;
+    printf("Addition :%d\n",*ptra+*ptrb);
+    printf("Substraction :%d\n",*ptra-*ptrb);
+}
